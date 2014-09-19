89d2597e4174d1a245020560ae1844d4d75f18b7
amercader <amercadero@gmail.com>
2014-09-19 22:30:16 +0100

    Add helper script for adding instances from the census

--- README.md
+++ README.md
@@ -4,3 +4,34 @@ How do I add my CKAN instance?
 ==============================
 
 Just add it to the [CKAN Census](http://ckan.org/census/)!
+
+
+Script for importing instances from the census
+==============================================
+
+TODO: Automate all this stuff!
+
+
+If you have push rights on this repo, you can import instances from a CSV export of the
+Census Google Spreadsheet using the `import_from_census.py` script.
+
+Assuming you exported the Census to `census.csv` and placed on the same directory, you
+can run:
+
+    python import_from_census.py
+
+By default, this will modify the `config/instances.json` file in place and download a
+screenshot of the new sites. Run `python import_from_census.py -h` to see all the available options.
+
+*Screenshots need to be resize manually*, as the screenshot service (http://webshot.okfnlabs.org)
+does not support it.
+
+You can use eg:
+
+    sudo apt-get install imagemagick
+    for file in *.png; do convert $file -resize 350x250 $file; done
+
+Review the changes and push them to the `gh-pages` branch to make them public.
+
+
+
---
+++ import_from_census.py
@@ -0,0 +1,155 @@
+import json
+import csv
+import urlparse
+import urllib
+import argparse
+
+
+def normalize_url(url):
+
+    url = url.lower().strip().rstrip('/')
+
+    if url == 'internal':
+        return None
+
+    if not url.startswith('http'):
+        url = '//' + url
+
+    parts = urlparse.urlparse(url, scheme='http')
+
+    return urlparse.urlunparse(parts)
+
+
+def get_instance_id_from_url(url):
+
+    url = normalize_url(url)
+
+    return (url.replace('https://', '')
+               .replace('http://', '')
+               .replace('.', '_')
+               .replace('/', '_'))
+
+
+def get_instance_id_from_title(title):
+
+    return title.lower().replace(' ', '_').replace('.', '_')
+
+
+def get_screenshot(url, output_path):
+
+    screenshot_service = 'http://webshot.okfnlabs.org/api/generate?url={url}&width={width}&height={heigth}'
+    request_url = screenshot_service.format(
+        url=url, width=1024, heigth=768
+    )
+    urllib.urlretrieve(request_url, output_path)
+
+    print 'Got screenshot for {0}'.format(url)
+    # TODO: resize
+    # For the time being, just do:
+    #   sudo apt-get install imagemagick
+    #   for file in *.png; do convert $file -resize 350x250 $file; done
+
+
+def add_new_instances_from_census(instances_file='config/instances.json',
+                                  new_instances_file='config/instances.json',
+                                  census_file='census.csv',
+                                  get_screenshots=True,
+                                  append=True):
+
+    with open(instances_file, 'r') as f:
+        instances = json.loads(f.read())
+
+    census_instances = []
+    with open(census_file, 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            census_instances.append(row)
+
+    instances_urls = [normalize_url(i['url']) for i in instances if i['url']]
+
+    instances_titles = [i['title'].encode('utf-8').strip() for i in instances]
+
+    missing_instances = []
+    for instance in census_instances:
+
+        url = normalize_url(instance['URL'])
+
+        if (url not in instances_urls
+                and instance['Site Name'].strip() not in instances_titles
+                and instance['CKAN.org Instances Page'] == 'Yes'):
+
+            if url:
+                instance_id = get_instance_id_from_url(instance['URL'])
+            else:
+                instance_id = get_instance_id_from_title(instance['Site Name'])
+
+            new_instance = {
+                'id': instance_id,
+                'url': url,
+                'title': instance['Site Name'],
+                'description': instance['Description'],
+                'location': instance['Location'],
+                'facets': [
+                    {'key': 'Region', 'value': instance.get('Region', '')},
+                    {'key': 'Type', 'value': instance.get('Type', '')}
+                ],
+            }
+
+            if url and get_screenshots:
+                output_path = 'images/instance/{0}.png'.format(instance_id)
+                get_screenshot(url, output_path)
+
+            missing_instances.append(new_instance)
+
+    if append:
+        instances.extend(missing_instances)
+        output_instances = instances
+    else:
+        output_instances = missing_instances
+
+    with open(new_instances_file, 'wb') as f:
+        f.write(
+            json.dumps(
+                output_instances,
+                indent=4,
+                separators=(',', ': ')
+            ))
+
+    print 'Done, {0} new instances added'.format(len(missing_instances))
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        description='Get new CKAN instances from the Census.')
+    parser.add_argument('-i', '--instances-file',
+                        default='config/instances.json',
+                        help='Existing instances JSON file (default: config/instances.json)')
+    parser.add_argument('-n', '--new-instances-file',
+                        default='config/instances.json',
+                        help='New instances JSON file (default: config/instances.json, ie overwrite)')
+    parser.add_argument('-c', '--census-file',
+                        default='census.csv',
+                        help='CKAN Census CSV file (default: census.csv)')
+    parser.add_argument('--screenshots', action='store_true',
+                        dest='get_screenshots',
+                        help='Get screenshots for new instances (default: true)')
+    parser.add_argument('--no-screenshots', action='store_false',
+                        dest='get_screenshots')
+    parser.add_argument('--append', action='store_true',
+                        dest='append',
+                        help='Append new instances to existing ones (default: true)')
+    parser.add_argument('--no-append', action='store_false',
+                        dest='append')
+
+    parser.set_defaults(add_region_facet=True,
+                        get_screenshots=True,
+                        append=True)
+
+    args = parser.parse_args()
+
+    add_new_instances_from_census(instances_file=args.instances_file,
+                                  new_instances_file=args.new_instances_file,
+                                  census_file=args.census_file,
+                                  get_screenshots=args.get_screenshots,
+                                  append=args.append)
